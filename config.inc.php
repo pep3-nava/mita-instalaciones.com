f93227af6788778d041b668b2ee32f8bd98df723
pep3-nava <118653041+pep3-nava@users.noreply.github.com>
2023-06-24 02:52:33 -0700

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+define("HOSTNAME_MTA_PUBLISH", "sdbemta04.simdif.local");
+define("EMAIL_CONFIRMED", true);
+define("SITE_URL", "https://mita-instalaciones.simdif.com");
+define("SENDER_EMAIL", "contact@simdif.com");
+define("SENDER_NAME", "contact");
+define("RECEIVER_EMAIL", "pepeenava9@gmail.com");
+define("RECEIVER_NAME", "");
+define("RECEIVER_LASTNAME", "");
+define("SUBJECT", "[sender_email] a trav&eacute;s de [site_name] : [form_subject]");
+define("CONTENT", "Tienes un mensaje de &lt;[sender_email]&gt;, a través de tu página de contacto en [site_name]");
+define("USER_ID", "1724005");
+define("SITE_ID", "2134313");
+define("ENV", "prod");
